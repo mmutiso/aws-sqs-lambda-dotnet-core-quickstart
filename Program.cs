e79bc35bf78ae27a5a2f9b2b319bd2ba0b5bf9ec
mmutiso <mutee88@gmail.com>
2020-06-03 22:56:53 +0300

    Added all files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Amazon.Lambda.Core;
+using System;
+
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Amazon.Lambda.SQSEvents;
+using System.Net.Mime;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace autoscale_lambda
+{
+    public class Program
+    {
+        public async Task  FunctionHandler(SQSEvent invocationEvent, ILambdaContext context)
+        {
+            await Task.FromResult(0);
+
+            LambdaLogger.Log("Context: " + JsonConvert.SerializeObject(context));
+            LambdaLogger.Log("Event: " + JsonConvert.SerializeObject(invocationEvent));
+        }
+    }
+}
